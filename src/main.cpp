262d54adae23ffbb40ba8fc0bfd39658646425a8
connor <ConnorKirkpatrick3@gmail.com>
2021-09-30 13:54:55 +0100

    Added a basic method for getting the distance. This is poor and often fluctuates.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
 #include <Arduino.h>
 
+
+#define triggerPin 2
+#define echoPin 3
+
+long duration;
+int distance;
+
+//5v to Vcc
+
+void getDistance();
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
+    getDistance();
     delay(1000);
 }
+
+void getDistance(){
+    //get the system ready
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(5);
+    //send out a pulse
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    //read the pulse
+    duration = pulseIn(echoPin, HIGH);
+    //calculate distance by the delay
+    distance = duration * 0.034 / 2;
+
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+}
+
+//TODO: query x times per second, remove outliers, take average
