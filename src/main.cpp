3a18648a9464e5961369078e3e9504c289a22ec3
connor <ConnorKirkpatrick3@gmail.com>
2021-09-30 13:38:49 +0100

    Added baseline GPS code. Uses the TINYGPS lib to enable GPS detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <TinyGPSPlus.h>
 
+SoftwareSerial gpsSerial(2,3);
+TinyGPSPlus GPS;
+
+float lat = 11.111, lon = 22.222;
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
+    while(gpsSerial.available()){ // check for gps data
+        if(GPS.encode(gpsSerial.read()))// encode gps data
+        {
+            Serial.print("LAT: ");
+            Serial.println(GPS.location.lat(),6);
+            Serial.print("LON: ");
+            Serial.println(GPS.location.lng(),6);
+            Serial.print("ALT(m): ");
+            Serial.println(GPS.altitude.meters(),6);
+        }
+    }
+    String latitude = String(lat,6);
+    String longitude = String(lon,6);
+    Serial.println(latitude+";"+longitude);
     delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
-    delay(1000);
+}
+
+void GPS_INIT(){
+    Serial.begin(9600);
+    gpsSerial.begin(9600);
+
 }
