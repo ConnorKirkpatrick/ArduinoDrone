2a561a99cecd82cb4957e1ef5caa574b85b7c8f0
connor <ConnorKirkpatrick3@gmail.com>
2021-09-30 18:11:10 +0100

    Added basic thread POC and notes on how to use going forward. Simple thread blinks the LED at 1 second intervals

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
 #include <Arduino.h>
+#include <pt.h>
+
+static struct pt pt1;
+
+static int protothreadBlinkLED1(struct pt *pt);
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    PT_INIT(&pt1);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
-    delay(1000);
+    protothreadBlinkLED1(&pt1);
 }
+
+static int protothreadBlinkLED1(struct pt *pt)
+{
+    static unsigned long lastTimeBlink = 0;
+    PT_BEGIN(pt);
+    while(1) {
+        lastTimeBlink = millis();
+        PT_WAIT_UNTIL(pt, millis() - lastTimeBlink > 1000);
+        digitalWrite(LED_BUILTIN, HIGH);
+        lastTimeBlink = millis();
+        PT_WAIT_UNTIL(pt, millis() - lastTimeBlink > 1000);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+    PT_END(pt);
+}
+
+
+///Notes:
+/// Dont use local variables with PT, use static as local are not preserved during a block
+/// any protothread function must have a pointer to the protothread as a parameter
+/// PT Begin and End start and stop the thread
+/// Use PT_WAIT_UNTIL(thread,condition), this will keep the thread paused until the condition is true
+/// Trick, you can call the wait immediately after an action is performed to preserve memory
+/// I will use this initially to blink the nav lights on the drone, later to expand the flight controllers autonomy
+/// DO NOT USE INTERRUPTS: these block the main process and can cripple the FC system
