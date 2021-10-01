aea631040a8676f6cf570369798355e8ecedfe8c
connor <ConnorKirkpatrick3@gmail.com>
2021-10-01 12:45:00 +0100

    Updated the code, Now utilises the correct TX,RX ports and can receive data from the GPS module Accuracy checked to be withing reasonable bounds so far (~50ft)

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,45 @@
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
 
-SoftwareSerial gpsSerial(2,3);
-TinyGPSPlus GPS;
+//Vcc 5v
+//use TX and RX rather than I2C
+//BAUD rate is 9600
 
-float lat = 11.111, lon = 22.222;
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+SoftwareSerial serial_connection(10, 11); //RX=pin 10, TX=pin 11
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+void setup()
+{
+    Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+    serial_connection.begin(9600);//This opens up communications to the GPS
+    Serial.println("GPS Start");//Just show to the monitor that the sketch has started
 }
 
-void loop() {
-    while(gpsSerial.available()){ // check for gps data
-        if(GPS.encode(gpsSerial.read()))// encode gps data
-        {
-            Serial.print("LAT: ");
-            Serial.println(GPS.location.lat(),6);
-            Serial.print("LON: ");
-            Serial.println(GPS.location.lng(),6);
-            Serial.print("ALT(m): ");
-            Serial.println(GPS.altitude.meters(),6);
-        }
+void loop()
+{
+    while(serial_connection.available())//While there are characters to come from the GPS
+    {
+        gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
     }
-    String latitude = String(lat,6);
-    String longitude = String(lon,6);
-    Serial.println(latitude+";"+longitude);
-    delay(1000);
+
+    if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+    {
+//Get the latest info from the gps object which it derived from the data sent by the GPS unit
+        Serial.println("GMT Time:");
+        Serial.println(gps.time.value());
+        Serial.println("Satellite Count:");
+        Serial.println(gps.satellites.value());
+        Serial.println("Latitude:");
+        Serial.println(gps.location.lat(), 6);
+        Serial.println("Longitude:");
+        Serial.println(gps.location.lng(), 6);
+        Serial.println("Speed MPH:");
+        Serial.println(gps.speed.mph());
+        Serial.println("Altitude Feet:");
+        Serial.println(gps.altitude.feet());
+        Serial.println("");
+    }
 }
 
-void GPS_INIT(){
-    Serial.begin(9600);
-    gpsSerial.begin(9600);
 
-}
+/// Best use case; offload the reading and encoding onto a protoThread
+/// Use the static GPS object to query for the coords at a more reasonable interval
