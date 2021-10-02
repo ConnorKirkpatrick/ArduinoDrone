af06e6a4d4f2ace7165bdb8ad832684034f9f80d
connor <ConnorKirkpatrick3@gmail.com>
2021-10-02 12:25:58 +0100

    Updated notes on wiring and board code

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Wire.h>
 
 //Vcc 5V
-
+//Wire I2C
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
