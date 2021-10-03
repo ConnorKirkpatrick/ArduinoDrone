1ba889ab2f5484bbc6eea172750086caa5ded8fd
connor <ConnorKirkpatrick3@gmail.com>
2021-10-03 22:06:34 +0100

    finished intergrating the GPS into a protothread Added basic startup check for the GPS, ensures the first fix with no sats is ignored

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,13 @@ void setup()
     PT_INIT(&pt1);
     PT_INIT(&pt2);
 
+    //Startup checks
+    while(coords[0] < 2){
+        getCoords(coords);
+    }
+
+    Serial.println("SYSTEM INITIALISED");
+
 }
 
 void loop()
@@ -41,27 +48,6 @@ void loop()
     PT_SCHEDULE(ProtoThread1(&pt1));
     PT_SCHEDULE(ProtoThread2(&pt2));
     PT_SCHEDULE(GPSThread(&gpsThread, coords, gps));
-
-/*    //query GPS
-    getCoords(coords);
-    //if there is no satellites, no valid fix can be made thus retry
-    while(coords[0] == 0){
-        getCoords(coords);
-    }
-    Serial.print("Sats: ");
-    Serial.println(coords[0],0);
-    Serial.print("Time: ");
-    Serial.println(coords[1],0);
-    Serial.print("LAT: ");
-    Serial.println(coords[2],6);
-    Serial.print("LNG: ");
-    Serial.println(coords[3],6);
-    Serial.print("Speed(m/s): ");
-    Serial.println(coords[4]);
-    Serial.print("altitude(m): ");
-    Serial.println(coords[5]);
-    Serial.println("\n\n\n");*/
-
 }
 
 void getCoords(float array[]) {
@@ -77,26 +63,6 @@ void getCoords(float array[]) {
     array[3] = gps.location.lng();
     array[4] = gps.speed.mps();
     array[5] = gps.altitude.meters();
-
-
-/*    if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
-    {
-
-//Get the latest info from the gps object which it derived from the data sent by the GPS unit
-        Serial.println("GMT Time:");
-        Serial.println(gps.time.value());
-        Serial.println("Satellite Count:");
-        Serial.println(gps.satellites.value());
-        Serial.println("Latitude:");
-        Serial.println(gps.location.lat(), 6);
-        Serial.println("Longitude:");
-        Serial.println(gps.location.lng(), 6);
-        Serial.println("Speed MPH:");
-        Serial.println(gps.speed.mph());
-        Serial.println("Altitude Feet:");
-        Serial.println(gps.altitude.feet());
-        Serial.println("");
-    }*/
 }
 
 
@@ -117,35 +83,23 @@ static int ProtoThread1(struct pt *pt)
         Serial.print("altitude(m): ");
         Serial.println(coords[5]);
         Serial.println("\n\n\n");
-        PT_YIELD(pt);
+        PT_SLEEP(pt,1500)
     }
-    PT_END(pt);
+    PT_END(pt)
 }
 static int ProtoThread2(struct pt *pt)
 {
-    static unsigned long lastTimeBlink = 0;
     PT_BEGIN(pt);
     while(1) {
-        Serial.println("Printing every 3 seconds");
-        PT_SLEEP(pt,3000)
+        Serial.println("Printing every 5 seconds");
+        PT_SLEEP(pt,5000)
     }
     PT_END(pt);
 }
 static int GPSThread(struct pt *pt,float array[], TinyGPSPlus GPS){
     PT_BEGIN(pt);
     while(1) {
-        while (!gps.location.isUpdated()) {
-            while (GPS_Connection.available())//While there are characters to come from the GPS
-            {
-                gps.encode(GPS_Connection.read());//This feeds the serial NMEA data into the library one char at a time
-            }
-        }
-        array[0] = gps.satellites.value();
-        array[1] = gps.time.value();
-        array[2] = gps.location.lat();
-        array[3] = gps.location.lng();
-        array[4] = gps.speed.mps();
-        array[5] = gps.altitude.meters();
+        getCoords(array);
         PT_SLEEP(pt,1000)
     }
     PT_END(pt)
