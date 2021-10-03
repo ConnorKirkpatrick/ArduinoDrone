67f60a08a25828e462b4a81bfc25706e5e0e2849
connor <ConnorKirkpatrick3@gmail.com>
2021-10-03 13:00:57 +0100

    Added recovery code, checks the status of the registers and decides whether there is a fault or not in the module

--- src/main.cpp
+++ src/main.cpp
@@ -7,45 +7,46 @@
 //connect via I2C ports
 //VCC 5v
 
+
 Adafruit_BMP280 bmp;
 
-[[noreturn]] void altimeter();
+void altimeter();
 
 void setup() {
     altimeter();
-    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
-    delay(1000);
+    Serial.println(bmp.getStatus());
+    //243 is a bad status->no gnd
+    //0 is also bad, need to re-init
+    //0Ã is bad, no power, can return when power is restored
+    //12,4 is good
+    if(bmp.getStatus() == 243 || bmp.getStatus() == 0){
+        altimeter();
+    }
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Approx altitude = ");
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(5000);
 }
 
-[[noreturn]] void altimeter(){
+void altimeter(){
     Serial.begin(9600);
     while(!bmp.begin(0x76)){
         Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
                          "try a different address!\n\n"));
         delay(3000);
     }
-    Serial.println("Good Connection");
-
-    while(true){
-        Serial.print("Temperature = ");
-        Serial.print(bmp.readTemperature());
-        Serial.println(" *C");
-
-        Serial.print("Pressure = ");
-        Serial.print(bmp.readPressure());
-        Serial.println(" Pa");
-
-        Serial.print("Approx altitude = ");
-        Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-        Serial.println(" m");
-
-        Serial.println();
-        delay(2000);
-    }
+    Serial.println("BMP-280 started");
 }
