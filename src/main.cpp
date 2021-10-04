c061aacd630de2fca9423d3da8497ca1ac6c06ba
connor <ConnorKirkpatrick3@gmail.com>
2021-10-04 21:49:46 +0100

    Added the altimeter, intergrated the startup sequence and also streamlined the method to get altitude

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <protothreads.h>
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
+#include <Adafruit_BMP280.h>
+
+
 
 static struct pt pt1;
 static struct pt pt2;
@@ -11,58 +14,48 @@ static int ProtoThread1(struct pt *pt);
 static int ProtoThread2(struct pt *pt);
 static int GPSThread(struct pt *pt,float array[], TinyGPSPlus GPS);
 
-
-
+//BME/P-280
+//connect via I2C ports
+//VCC 5v
+Adafruit_BMP280 bmp;
 
 //Vcc 5v
 //use TX and RX rather than I2C
 //BAUD rate is 9600
-
-float coords[6];
-
 SoftwareSerial GPS_Connection(10, 11); //RX=pin 10, TX=pin 11
 TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+float coords[6];
 
+void altimeter();
 void getCoords(float array[]);
 
 void setup()
 {
+    delay(5000);
+    Serial.begin(9600);
     Serial.println("SYSTEM INITIALISING");
-    Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
-    GPS_Connection.begin(9600);//This opens up communications to the GPS
+
 
     PT_INIT(&pt1);
     PT_INIT(&pt2);
 
+    GPS_Connection.begin(9600);//This opens up communications to the GPS
     //Startup checks
     while(coords[0] < 2){
         getCoords(coords);
     }
     Serial.println("GPS STARTED");
-
+    altimeter();
     Serial.println("SYSTEM INITIALISED");
-
 }
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BMP280.h>
-
-//BME/P-280
-//connect via I2C ports
-//VCC 5v
 
 
-Adafruit_BMP280 bmp;
 
-void altimeter();
 
-void loop()
-{
+void loop() {
     PT_SCHEDULE(ProtoThread1(&pt1));
     PT_SCHEDULE(ProtoThread2(&pt2));
     PT_SCHEDULE(GPSThread(&gpsThread, coords, gps));
-void setup() {
-    altimeter();
 }
 
 void getCoords(float array[]) {
@@ -78,49 +71,13 @@ void getCoords(float array[]) {
     array[3] = gps.location.lng();
     array[4] = gps.speed.mps();
     array[5] = gps.altitude.meters();
-void loop() {
-    Serial.println(bmp.getStatus());
-    //243 is a bad status->no gnd, no recovery
-    //0 is also bad, need to re-init
-    //0Ã is bad, no power, can return when power is restored
-    //12,4 is good
-    if(bmp.getStatus() == 243 || bmp.getStatus() == 0){
-        altimeter();
-    }
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print("Approx altitude = ");
-    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-    Serial.println(" m");
-
-    Serial.println();
-    delay(5000);
 }
-
 
 static int ProtoThread1(struct pt *pt)
 {
     PT_BEGIN(pt);
     while(1) {
-        Serial.print("Sats: ");
-        Serial.println(coords[0],0);
-        Serial.print("Time: ");
-        Serial.println(coords[1],0);
-        Serial.print("LAT: ");
-        Serial.println(coords[2],6);
-        Serial.print("LNG: ");
-        Serial.println(coords[3],6);
-        Serial.print("Speed(m/s): ");
-        Serial.println(coords[4]);
-        Serial.print("altitude(m): ");
-        Serial.println(coords[5]);
-        Serial.println("\n\n\n");
+
         PT_SLEEP(pt,1500)
     }
     PT_END(pt)
@@ -159,6 +116,7 @@ static int GPSThread(struct pt *pt,float array[], TinyGPSPlus GPS){
 /// Use the static GPS object to query for the coords at a more reasonable interval
 /// On start, wait until the GPS is acquired before taking flights
 
+
 void altimeter(){
     Serial.begin(9600);
     while(!bmp.begin(0x76)){
@@ -167,4 +125,30 @@ void altimeter(){
         delay(3000);
     }
     Serial.println("BMP-280 started");
+}
+
+float getAltitude(){
+    Serial.println(bmp.getStatus());
+    //243 is a bad status->no gnd, no recovery
+    //0 is also bad, need to re-init
+    //0Ã is bad, no power, can return when power is restored
+    //12,4 is good
+    if(bmp.getStatus() == 243 || bmp.getStatus() == 0){
+        altimeter();
+    }
+
+/*    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Approx altitude = ");
+    Serial.print(bmp.readAltitude(1013.25)); *//* Adjusted to local forecast! *//*
+    Serial.println(" m");
+
+    Serial.println();*/
+    return bmp.readAltitude();
 }
