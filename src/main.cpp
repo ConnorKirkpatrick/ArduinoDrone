cc4150d31f5782991d183f1b2484d10c0c2d7823
connor <ConnorKirkpatrick3@gmail.com>
2021-10-10 19:44:21 +0100

    Added code for the voltage sensors basic operation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
 
+//NO VCC IN
+//connect ground and the sensor pin
+
+int offset = 0;
+int volt = 0;
+double voltage = 0;
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    pinMode(50, OUTPUT);
+    pinMode(A0,INPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(50,LOW);
+    volt = analogRead(A0);
+    voltage = map(volt,0,1023, 0, 2500) + offset;
+    voltage /=100;
+
+    Serial.println(voltage);
     delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
+
+    digitalWrite(50,HIGH);
+    volt = analogRead(A0);
+    voltage = map(volt,0,1023, 0, 2500) + offset;
+    voltage /=100;
+
+    Serial.println(voltage);
     delay(1000);
+
 }
