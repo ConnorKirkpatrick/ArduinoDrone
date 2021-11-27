64db85cefd6880769f2cad467157ac24b450b390
connor <ConnorKirkpatrick3@gmail.com>
2021-11-27 15:30:10 +0000

    Updated the gyro to use the Adafruit library and to give actual rotation rather than rate of

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,164 @@
-#include <Arduino.h>
+#include "Adafruit_MPU6050.h"
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
-//GY-251
-//Vcc 5V
-//Wire I2C
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+#include "Arduino.h"
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
-int16_t temperature; // variables for temperature data
+Adafruit_MPU6050 mpu;
 
-char tmp_str[7]; // temporary variable used in convert function
+double adjust_x = -8.26;
+double adjust_y = -2.55;
+double adjust_z = 0;
 
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
-    sprintf(tmp_str, "%6d", i);
-    return tmp_str;
-}
-
-void setup() {
+void setup(void) {
     Serial.begin(9600);
-    Wire.begin();
-    Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-    Wire.write(0x6B); // PWR_MGMT_1 register
-    Wire.write(0); // set to zero (wakes up the MPU-6050)
-    Wire.endTransmission(true);
+    while (!Serial)
+        delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+    Serial.println("Adafruit MPU6050 test!");
+
+    // Try to initialize!
+    if (!mpu.begin()) {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1) {
+            delay(10);
+        }
+    }
+    Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange()) {
+        case MPU6050_RANGE_2_G:
+            Serial.println("+-2G");
+            break;
+        case MPU6050_RANGE_4_G:
+            Serial.println("+-4G");
+            break;
+        case MPU6050_RANGE_8_G:
+            Serial.println("+-8G");
+            break;
+        case MPU6050_RANGE_16_G:
+            Serial.println("+-16G");
+            break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange()) {
+        case MPU6050_RANGE_250_DEG:
+            Serial.println("+- 250 deg/s");
+            break;
+        case MPU6050_RANGE_500_DEG:
+            Serial.println("+- 500 deg/s");
+            break;
+        case MPU6050_RANGE_1000_DEG:
+            Serial.println("+- 1000 deg/s");
+            break;
+        case MPU6050_RANGE_2000_DEG:
+            Serial.println("+- 2000 deg/s");
+            break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth()) {
+        case MPU6050_BAND_260_HZ:
+            Serial.println("260 Hz");
+            break;
+        case MPU6050_BAND_184_HZ:
+            Serial.println("184 Hz");
+            break;
+        case MPU6050_BAND_94_HZ:
+            Serial.println("94 Hz");
+            break;
+        case MPU6050_BAND_44_HZ:
+            Serial.println("44 Hz");
+            break;
+        case MPU6050_BAND_21_HZ:
+            Serial.println("21 Hz");
+            break;
+        case MPU6050_BAND_10_HZ:
+            Serial.println("10 Hz");
+            break;
+        case MPU6050_BAND_5_HZ:
+            Serial.println("5 Hz");
+            break;
+    }
+
+    Serial.println("");
+    delay(100);
 }
 
 void loop() {
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-    Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-    Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-    // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-    accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-    accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-    accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-    temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-    gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-    gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-    gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
 
-    // print out data
-    Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-    Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-    Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-    // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-    Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-    Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-    Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-    Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
-    Serial.println();
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
 
-    // delay
-    delay(1000);
+    const int MPU_addr=0x68;
+    int16_t AcX,AcY,AcZ;
+
+    double x;
+    double y;
+    double z;
+
+    int minVal=265;
+    int maxVal=402;
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);
+    AcX=Wire.read()<<8|Wire.read();
+    AcY=Wire.read()<<8|Wire.read();
+    AcZ=Wire.read()<<8|Wire.read();
+    Wire.end();
+    int xAng = map(AcX,minVal,maxVal,-90,90);
+    int yAng = map(AcY,minVal,maxVal,-90,90);
+    int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+    x= (RAD_TO_DEG * (atan2(-yAng, -zAng)+PI))-adjust_x;
+    y= (RAD_TO_DEG * (atan2(-xAng, -zAng)+PI))-adjust_y;
+    z= (RAD_TO_DEG * (atan2(-yAng, -xAng)+PI))-adjust_z;
+    if(x>359.99){x = x-360;}
+    if(y>359.99){y = y-360;}
+    if(z>359.99){z = z-360;}
+
+    if(x<0){x = x+360;}
+    if(y<0){y = y+360;}
+    if(z<0){z = z+360;}
+
+
+    Serial.print("AngleX= ");
+    Serial.println(x);
+
+    Serial.print("AngleY= ");
+    Serial.println(y);
+
+    Serial.print("AngleZ= ");
+    Serial.println(z);
+    Serial.println("-----------------------------------------");
+
+
+
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    Serial.println("");
+    delay(50);
 }
+
