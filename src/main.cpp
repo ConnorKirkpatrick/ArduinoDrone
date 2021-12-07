5dd2edd7163fca1881fad0b9ec60f45defd9f9bd
connor <ConnorKirkpatrick3@gmail.com>
2021-12-07 17:08:54 +0000

    Added timing and distance system to try to estimate the speed of the module more accurately

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,14 @@
 //use TX and RX rather than I2C
 //BAUD rate is 9600
 
-float coords[6];
+const double EIFFEL_TOWER_LAT = 48.85826;
+const double EIFFEL_TOWER_LNG = 2.294516;
 
+float coords[6];
+float lastFix[2];
+float oldTime = 0;
+float newTime = 0;
+double distanceFromLastFix;
 TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
 
 void getCoords(float array[]);
@@ -23,66 +29,64 @@ void setup()
 
 void loop()
 {
-    Serial.println(Serial1.read());
     //query GPS
     getCoords(coords);
     //if there is no satellites, no valid fix can be made thus retry
     while(coords[0] == 0){
         getCoords(coords);
     }
-    Serial.print("Sats: ");
-    Serial.println(coords[0],0);
-    Serial.print("Time: ");
-    Serial.println(coords[1],0);
-    Serial.print("LAT: ");
-    Serial.println(coords[2],6);
-    Serial.print("LNG: ");
-    Serial.println(coords[3],6);
-    Serial.print("Speed(m/s): ");
-    Serial.println(coords[4]);
-    Serial.print("altitude(m): ");
-    Serial.println(coords[5]);
-    Serial.println("\n\n\n");
-    delay(1000);
+    if(coords[1] != oldTime){
+        Serial.print("Sats: ");
+        Serial.println(coords[0],0);
+        Serial.print("Time: ");
+        Serial.println(coords[1],0);
+        newTime = coords[1];
+        Serial.print("LAT: ");
+        Serial.println(coords[2],6);
+        Serial.print("LNG: ");
+        Serial.println(coords[3],6);
+        Serial.print("Speed(m/s): ");
+        Serial.println(coords[4]);
+        Serial.print("altitude(m): ");
+        Serial.println(coords[5]);
+        Serial.println("");
+        //distanceFromLastFix = gps.distanceBetween(EIFFEL_TOWER_LAT,EIFFEL_TOWER_LNG,coords[2],coords[3]);
+        distanceFromLastFix = gps.distanceBetween(lastFix[0],lastFix[1],coords[2],coords[3]);
+        Serial.print("Distance since last fix: ");
+        Serial.println(distanceFromLastFix);
+        lastFix[0] = coords[2];
+        lastFix[1] = coords[3];
+        Serial.println("");
+        Serial.print("Time passed: ");
+        int timePassed = (newTime - oldTime)/100;
+        Serial.print(timePassed);
+        Serial.println(" Seconds");
+        oldTime = newTime;
+        Serial.print("Speed: ");
+        Serial.print(distanceFromLastFix/timePassed);
+        Serial.println(" M/S");
+        Serial.println("\n\n\n");
+    }
+
 }
 
 void getCoords(float array[]){
-    Serial.println(Serial1.read());
-    while(!gps.location.isUpdated()){
+    if(Serial1.available()){
         while(Serial1.available())//While there are characters to come from the GPS
         {
             gps.encode(Serial1.read());//This feeds the serial NMEA data into the library one char at a time
         }
     }
-    array[0] = gps.satellites.value();
-    array[1] = gps.time.value();
-    array[2] = gps.location.lat();
-    array[3] = gps.location.lng();
-    array[4] = gps.speed.mps();
-    array[5] = gps.altitude.meters();
-
-
-/*    if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
-    {
-
-//Get the latest info from the gps object which it derived from the data sent by the GPS unit
-        Serial.println("GMT Time:");
-        Serial.println(gps.time.value());
-        Serial.println("Satellite Count:");
-        Serial.println(gps.satellites.value());
-        Serial.println("Latitude:");
-        Serial.println(gps.location.lat(), 6);
-        Serial.println("Longitude:");
-        Serial.println(gps.location.lng(), 6);
-        Serial.println("Speed MPH:");
-        Serial.println(gps.speed.mph());
-        Serial.println("Altitude Feet:");
-        Serial.println(gps.altitude.feet());
-        Serial.println("");
-    }*/
-
+    if(gps.location.isUpdated()){
+        array[0] = gps.satellites.value();
+        array[1] = gps.time.value();
+        array[2] = gps.location.lat();
+        array[3] = gps.location.lng();
+        array[4] = gps.speed.mps();
+        array[5] = gps.altitude.meters();
+    }
 }
-
+//if age > 1500 consider lost fix
 
 
 /// Best use case; offload the reading and encoding onto a protoThread
