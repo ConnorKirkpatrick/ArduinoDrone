b5881bd5841d31f06d8730ae696dd9cdc042acb8
ConnorKirkpatrick <58564156+ConnorKirkpatrick@users.noreply.github.com>
2022-02-08 15:14:44 +0000

    Change PWM reading function to be compatible with the DUE
    Changed from PulseIn() to interrupts as the 5v pwm from the rc receiver would not give stable values, fluctuated over 200 units

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 
 //requires system be wired into PWM ports
+volatile int pwm_value = 0;
+volatile int prev_time = 0;
+ 
 void setup() {
-    pinMode(2, INPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
+  // when pin D2 goes high, call the rising function
+  attachInterrupt(digitalPinToInterrupt(12), rising, RISING);
+}
+ 
+void loop() { }
+//Use Interups on the DUE as reading the 5v pwm from the RC reciever is flakey using pulseIn()
+void rising() {
+  attachInterrupt(digitalPinToInterrupt(12), falling, FALLING);
+  prev_time = micros();
+}
+ 
+void falling() {
+  attachInterrupt(digitalPinToInterrupt(12), rising, RISING);
+  pwm_value = micros()-prev_time - 5;
+  if(pwm_value < 1000){pwm_value = 1000;}
+  Serial.println(pwm_value);
 }
 
-void loop() {
-    int data = 0;
-    int ch = pulseIn(2, HIGH, 30000);
-    Serial.println(ch);
-    data =  map(ch, 1000, 2000, 0, 100);
-    Serial.println(data);
-    delay(500);
-}
 /* ch1 is roll
  * ch2 is pitch
  * ch3 is throttle
