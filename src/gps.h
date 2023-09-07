05cc7a90535350b0eb916d005947d432b673263d
Connor <connorKirkpatrick3@gmail.com>
2023-09-07 16:05:34 +0100

    Updated GPS flow, still WIP due to hardware bugs

--- src/gps.h
+++ src/gps.h
@@ -2,38 +2,68 @@
 //use TX and RX rather than I2C
 //BAUD rate is 9600
 #include "TinyGPS++.h"
-TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
-double coords[6];
 
+TinyGPSPlus GPS;
 
-void getCoords(double array[]){
-  if(Serial1.available()){
-    while(Serial1.available())//While there are characters to come from the GPS
-    {
-      gps.encode(Serial1.read());//This feeds the serial NMEA data into the library one char at a time
+int satsInView = 0;
+TinyGPSCustom VDOP(GPS,"GPGSA", 18);
+TinyGPSCustom EAlt(GPS, "GPGGA", 12);
+TinyGPSCustom SV(GPS, "GPGSV", 3);
+
+void getGPSData(){
+    unsigned char x;
+    String line = "";
+    if(Serial1.available()){
+        while(Serial1.available())//While there are characters to come from the GPS
+        {
+            GPS.encode(Serial1.read());//This feeds the serial NMEA data into the library one char at a time
+        }
     }
-  }
-  if(gps.location.isUpdated()){ //if data is not updated, don't waste time getting a new set, just wait for the next request
-    array[0] = gps.satellites.value();
-    array[1] = gps.time.value();
-    array[2] = gps.location.lat();
-    array[3] = gps.location.lng();
-    array[4] = gps.speed.mps();
-    array[5] = gps.altitude.meters();
-  }
 }
 
-void startGPS(){
-  sendRadio("GPS Starting....");
-  Serial1.begin(9600);
-  getCoords(coords);
+
+void startGPS() {
+    sendStatusMessage("GPS Starting....", SEVERITY_INFO);
+    getGPSData();
+    while(GPS.location.lng() == 0.00f ) {
+        getGPSData();
+        int fixType, sats;
+        if((int) GPS.satellites.value() > atoi(SV.value())){
+            sats = (int) GPS.satellites.value();
+        }
+        else{
+            sats = atoi(SV.value());
+        }
+        if(sats < 3){
+            fixType =1;
+        }
+        else if(sats < 4){
+            fixType = 2; //2D fix
+        }
+        else{
+            fixType = 3; //3D fix
+        }
+        sendGPSRaw(fixType,GPS.location.lat(), GPS.location.lng(), GPS.altitude.meters(), GPS.hdop.value(), atoi(VDOP.value()), GPS.speed.mps(), GPS.course.deg(), sats, atoi(EAlt.value()));
+        for(int i = 0; i < 5; i++){ //delay 5s but still read GPS updates
+            delay(1000);
+            getGPSData();
+        }
+
+    }
+    sendGPSRaw(2,GPS.location.lat(), GPS.location.lng(), GPS.altitude.meters(), GPS.hdop.value(), atoi(VDOP.value()), GPS.speed.mps(), GPS.course.deg(), GPS.satellites.value(), atoi(EAlt.value()));
+    sendStatusMessage("GPS Ready", SEVERITY_INFO);
+}
+
+
+
   //if there is no satellites, no valid fix can be made thus retry
+  /*
   while(coords[0] < 1) {
     delay(2000);
     getCoords(coords);
     String data = "Found satellites: ";
     data += coords[0];
-    sendRadio(data);
+    //sendRadio(data);
   }
-  sendRadio("GPS Ready");
-}
+   */
+  //sendRadio("GPS Ready");
