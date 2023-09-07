9ed801276e61b6ecc106bb2c988844e3058573c3
Connor <connorKirkpatrick3@gmail.com>
2023-09-07 16:04:46 +0100

    Changed communication methods to utilise the new radio methods. Added filter for heading

--- src/kalmanFilters.h
+++ src/kalmanFilters.h
@@ -2,10 +2,11 @@
 
 Kalman kalmanX; // Create the Kalman instances
 Kalman kalmanY;
+Kalman kalmanZ;
 
 Kalman kalmanAX, kalmanAY, kalmanAZ;
 
-double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+double kalAngleX, kalAngleY, kalHeading; // Calculated angle using a Kalman filter
 double kalAccX, kalAccY, kalAccZ; //Acceleration using kalman filter
 
 void setupFilters(attitude currentAttitude){
@@ -17,28 +18,36 @@ void setupFilters(attitude currentAttitude){
   kalmanAZ.setAngle(currentAttitude.AccZ);
 }
 
-void kalmanUpdate(attitude currentAttitude, double dt){
-  ///Kalman filter for pitch
-  if ((currentAttitude.pitch < -90 && kalAngleX > 90) || (currentAttitude.pitch > 90 && kalAngleX < -90)) {
-    kalmanX.setAngle(currentAttitude.pitch);
-    kalAngleX = currentAttitude.pitch;
-  } else {
-    kalAngleX = kalmanX.getAngle(
+void kalmanUpdate(attitude currentAttitude,double heading, double dt){
+    ///Kalman filter for pitch
+    if ((currentAttitude.pitch < -90 && kalAngleX > 90) || (currentAttitude.pitch > 90 && kalAngleX < -90)) {
+        kalmanX.setAngle(currentAttitude.pitch);
+        kalAngleX = currentAttitude.pitch;
+        }
+    else {
+        kalAngleX = kalmanX.getAngle(
         currentAttitude.pitch, currentAttitude.RAccY, dt); // Calculate the angle using a Kalman filter
-  }
-  if (abs(kalAngleX) > 90)
-    currentAttitude.RAccY = -currentAttitude.RAccY; // Invert rate, so it fits the restricted accelerometer reading
+    }
+    if (abs(kalAngleX) > 90){
+        currentAttitude.RAccY = -currentAttitude.RAccY; // Invert rate, so it fits the restricted accelerometer reading
+    }
 
-  ///Kalman filter for Roll
-  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-  if ((currentAttitude.roll < -90 && kalAngleY > 90) || (currentAttitude.roll > 90 && kalAngleY < -90)) {
-    kalmanY.setAngle(currentAttitude.roll);
-    kalAngleY = currentAttitude.roll;
-  } else{
-    kalAngleY = kalmanY.getAngle(currentAttitude.roll, currentAttitude.RAccX, dt); // Calculate the angle using a Kalman filter
-  }
-  if (abs(kalAngleX) > 90)
-    currentAttitude.RAccX = -currentAttitude.RAccX; // Invert rate, so it fits the restricted accelerometer reading
+    ///Kalman filter for Roll
+    // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+    if ((currentAttitude.roll < -90 && kalAngleY > 90) || (currentAttitude.roll > 90 && kalAngleY < -90)) {
+        kalmanY.setAngle(currentAttitude.roll);
+        kalAngleY = currentAttitude.roll;
+    }
+    else{
+        kalAngleY = kalmanY.getAngle(currentAttitude.roll, currentAttitude.RAccX, dt); // Calculate the angle using a Kalman filter
+    }
+    if (abs(kalAngleX) > 90){
+        currentAttitude.RAccX = -currentAttitude.RAccX; // Invert rate, so it fits the restricted accelerometer reading
+    }
+
+    ///kalman filter for heading
+    kalHeading = kalmanZ.getAngle(heading, currentAttitude.RAccZ, dt);
+
 
   ///Kalman filter for directional acceleration
   kalAccX = kalmanAX.getAngle(currentAttitude.AccX, currentAttitude.AccX,dt);
