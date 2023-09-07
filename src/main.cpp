67747e503ab04063b89bfeefae40d287a1629dec
Connor <connorKirkpatrick3@gmail.com>
2023-09-07 16:06:23 +0100

    Updated to run new firmware to utilise new methods and comms equipment

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,143 @@
 #include "Arduino.h"
+#include "MadgwickAHRS.h"
 
 /// file includes
-#include "LoRa_Radio.h"
+#include "Radio.h"
 #include "gps.h"
 #include "gyro.h"
 #include "kalmanFilters.h"
 #include "compass.h"
 #include "barometer.h"
+#include "AHRS.h"
 
-Serial_ Radio;
 uint32_t timer;
-int rdt = 0;
+int lastMessage = 0;
+double state[9];
+
+Madgwick filter;
+unsigned long microsPerReading, microsPrevious, microsNow;
+float pitch, roll, yaw, heading;
+
+
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting up");
+    Serial.begin(115200);
+    Serial1.begin(9600);
+    microsPerReading = 1000000 / 25;
+    microsPrevious = micros();
+    startRadio();
+    delay(1000);// Wait for sensor to stabilize
+    //startBMP();
+    startCompass();
+    startGyro();
+    startGPS();
+    getGyroData();
+    setupFilters(currentAttitude);
+    timer = micros();
+    sendStatusMessage("System components started successfully",SEVERITY_INFO);
 
-  startRadio();
-
-  delay(1000);// Wait for sensor to stabilize
-  startBMP();
-  startCompass();
-  startGyro();
-  //startGPS();
-  getGyroData();
-  setupFilters(currentAttitude);
-  timer = micros();
-
-  Serial.println("SYSTEM STARTED");
-  sendRadio("System startup complete");
-  //now send the initialisation data to the base station:
-  String data = "1,";
-  //data += String(coords[2],7);
-  data += "51.3443708";
-  data += ",";
-  data += "-0.6433905";
-  //data += String(coords[3],7);
-  data += ",";
-  data += kalAngleX * 180 / PI;
-  data += ",";
-  data += kalAngleY * 180 / PI;
-  data += ",";
-  data += getHeading(currentAttitude);
-  data += ",";
-  data += getAltitude();
-  sendRadio(data);
-
+    double init_data[9];
+    init_data[0] = GPS.location.lat();
+    init_data[1] = GPS.location.lng();
+    init_data[2] = GPS.altitude.meters();
+    init_data[3] = 0;
+    init_data[4] = 0;
+    init_data[5] = 0;
+    init_data[6] = 0;
+    init_data[7] = 0;
+    init_data[8] = 0;
+    start(init_data);
+    sendStatusMessage("System started successfully",SEVERITY_INFO);
+    sendGlobalPosition(X[0],X[1],X[2],X[2],X[3],X[4],X[5],headingDegrees);
+    sendAttitude(currentAttitude.roll,currentAttitude.pitch, currentAttitude.yaw, currentAttitude.RAccY, currentAttitude.RAccX, currentAttitude.RAccZ);
+    lastMessage = millis();
 }
 
 void loop() {
 
-  ///Update all the values
-  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
-  timer = micros();
+    ///Update all the values
+    double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+    timer = micros();
 
-  currentAttitude = getGyroData();
-  kalmanUpdate(currentAttitude, dt);
-  getCoords(coords);
-  if(timer/1000 - rdt >= 500){ //send data every 0.5 seconds
-    //it has been 1s since last transmission
-    rdt = timer/1000;
-    //data format: lat,long,pitch,roll,heading,alt
-    String data = "2,";
-    //data += String(coords[2],7);
-    data += "51.3443708";
-    data += ",";
-    data += "-0.6433905";
-    //data += String(coords[3],7);
-    data += ",";
-    data += kalAngleX * 180 / PI;
-    data += ",";
-    data += kalAngleY * 180 / PI;
-    data += ",";
-    data += getHeading(currentAttitude);
-    data += ",";
-    data += getAltitude();
-    sendRadio(data);
-  }
+    currentAttitude = getGyroData();
+    kalmanUpdate(currentAttitude, heading, dt); //pass data to kalman filters to get accurate pitch+roll data
+
+    getGPSData();
+    updateAHRS(currentAttitude, heading, deltaT);
+    double data[9];
+    if(GPS.location.isUpdated()){
+        data[0] = GPS.location.lat();
+        data[1] = GPS.location.lng();
+        data[2] = GPS.altitude.meters();
+        data[3] = northV;
+        data[4] = eastV;
+        data[5] = upV;
+        data[6] = northA;
+        data[7] = eastA;
+        data[8] = upA;
+        predict(millis());
+        update(data);
+        //move to using kalman values for accelerations to combat upV constant increase
+    }
+    else{
+        data[0] = X[0];
+        data[1] = X[1];
+        data[2] = X[2];
+        data[3] = northV;
+        data[4] = eastV;
+        data[5] = upV;
+        data[6] = northA;
+        data[7] = eastA;
+        data[8] = upA;
+        predict(millis());
+        update(data);
+    }
+
+    if(millis() - lastMessage > 250){
+        //send status messages
+        sendGlobalPosition(X[0],X[1],X[2],X[2],X[3],X[4],X[5],headingDegrees);
+        sendAttitude(kalAngleY,kalAngleX, currentAttitude.yaw, kalAccY, kalAccX, kalAccZ);
+        int fixType, sats;
+        sats = (int) SV.value();
+
+        if(sats < 3){
+            fixType = 1;
+        }
+        else if(sats < 4){
+            fixType = 2; //2D fix
+        }
+        else{
+            fixType = 3; //3D fix
+        }
+        if(GPS.location.age() < 300){
+            sendStatusMessage(String(SV.isValid()),SEVERITY_NOTICE);
+            sendGPSRaw(fixType,GPS.location.lat(), GPS.location.lng(), GPS.altitude.meters(), GPS.hdop.value(), atoi(VDOP.value()), GPS.speed.mps(), GPS.course.deg(), sats, atoi(EAlt.value()));
+        }
+        lastMessage = millis();
+    }
+
+    /*
+    currentAttitude = getGyroData(); //grab the data from the IMU
+    kalmanUpdate(currentAttitude, dt); //pass data to kalman filters to get accurate pitch+roll data
+    Serial.print("Pitch:");
+    Serial.print(currentAttitude.RAccX);
+    Serial.print(",");
+
+    Serial.print("Max:");
+    Serial.print(2*PI);
+    Serial.print(",");
+
+    Serial.print("Min:");
+    Serial.println(-2*PI);
+    /*
+    Serial.print(",");
+
+    Serial.print("Roll:");
+    Serial.println(kalAngleY);
+     */
+
+
 }
 // 51.3443708,-0.6433905,134.11,-129.55,224.68,0.00
+
+// Send statustext messages about systems, then GPS_INT for attitude data
